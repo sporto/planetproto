43abeeb4872920d09fc07052a63d5e4a70379760
Sebastian Porto <sebasporto@gmail.com>
2015-01-22 16:49:53 +1100

    Fixed typo

--- exercises/070_dot_new/boilerplate/070.js
+++ exercises/070_dot_new/boilerplate/070.js
@@ -1,6 +1,6 @@
 // -> Define an object called 'Robot'
 // -> Define a method called 'new' in Robot
-// -> When Robot.new is called is should return a new object with Robot as its prototype 
+// -> When Robot.new is called it should return a new object with Robot as its prototype 
 //    e.g. var robby = Robot.new();
 //    Robot should be the prototype of robby
 
